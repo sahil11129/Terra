7cc0f43cc8c050f36897d01f711c9dbc55a8e257
sahil11129 <112084296+sahil11129@users.noreply.github.com>
2022-11-14 15:49:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,18 @@ terraform {
   }
 }
 
+module "dev_infrastructure_watsonassistant" {
+  source = "github.com/ibm-garage-cloud/terraform-service-watsonassistant?ref=v1.0.0"
 
+  resource_group_name = module.dev_cluster.resource_group_name
+  resource_location   = module.dev_cluster.region
+  cluster_id          = module.dev_cluster.id
+  namespaces          = module.dev_cluster_namespaces.release_namespaces
+  namespace_count     = var.release_namespace_count
+  name_prefix         = var.name_prefix
+  tags                = []
+  plan                = "standard"
+}
 
 
 
