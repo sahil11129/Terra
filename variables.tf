fea7bd00252769bc9108bfda486660ab09285038
sahil11129 <112084296+sahil11129@users.noreply.github.com>
2022-11-15 16:28:52 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -54,3 +54,21 @@ variable "cloudTarget" {
   type    = string
   default = ""
 }
+
+
+
+variable "ibmcloud_api_key" {
+  description = "IBM Cloud API Key"
+  type = string
+}
+
+variable "cluster_name" {
+  description = "Cluster Name/ID"
+  type = string
+}
+
+variable "storage_size" {
+  description = "NFS Size"
+  type        = number
+  default     = 0
+}
